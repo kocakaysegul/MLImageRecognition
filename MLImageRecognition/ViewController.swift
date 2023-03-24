7dd7d4dda7d6c53c102a70b6c20c5b9ff0d2af22
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-24 05:53:28 +0300

    Initial Commit

---
+++ MLImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLImageRecognition
+//
+//  Created by Ayşegül Koçak on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
